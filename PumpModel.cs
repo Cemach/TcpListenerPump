2b8e58c985e7c479dd9f29761350471951c8bdbd
CemachMD <cesar@md-online.com.br>
2021-07-07 18:42:42 -0300

    First commit Simulation Variables Pump

---
+++ PumpModel.cs
@@ -0,0 +1,14 @@
+public class PumpModel{
+        public double InletPressure;
+        public double OutletPressure;
+        public double Temperature;
+        public double Velocity;
+
+        public PumpModel()
+        {
+            this.InletPressure = 50;
+            this.OutletPressure = 101;
+            this.Temperature = 52;
+            this.Velocity = 100;
+        }
+    }
